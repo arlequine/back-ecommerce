42254a1f4e0c4efd372706132289859f48de5387
Alfredo Hernandez Noriega <alfredo.hernandez@kushkipagos.com>
2023-04-22 10:09:27 -0600

    feat(products): add products route api

--- apis/index.js
+++ apis/index.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const router = express.Router()
 const userRouter = require('./user')
+const productRouter = require('./product')
 const authRouter = require('./auth')
 const authMiddleware = require('../middleware/authorization')
 const registerRouter = require('./register')
 
 router.use('/auth', authRouter)
 router.use('/register', registerRouter)
+router.use('/products', productRouter)
 
 router.use(authMiddleware)
 router.use('/users', userRouter)
